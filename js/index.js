d5d6d3ab51c1fab0c4e32f124ee995cdf14460e2
Nazarenko Yurii <wallpapeforpc@gmail.com>
2023-09-17 16:12:28 +0300

    feat: add search method and season suggestions

--- js/index.js
+++ js/index.js
@@ -2,25 +2,35 @@ const date = new Date();
 const gallery = document.getElementById("gallery");
 const searchInput = document.getElementById("search-bar");
 const searchIcon = document.getElementById("search-icon");
+const seasonBtn = document.getElementById("season-suggestion");
 const arrayOfSeasons = ["winter", "spring", "summer", "autumn"];
-const season = arrayOfSeasons[getSeason()];
-const myUrl =
-  "https://api.unsplash.com/photos/?client_id=588sChbD4XYfVMX6oxBTVOtqczMXoFRFt2eYmCEkglk";
-const seasonUrl = '';
 const url =
-  "https://api.unsplash.com/photos?client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo";
-const testUrl = 'https://api.unsplash.com/photos?&per_page=30&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo';
+  "https://api.unsplash.com/photos?&per_page=30&client_id=588sChbD4XYfVMX6oxBTVOtqczMXoFRFt2eYmCEkglk";
 
+// GET INFO FUNCTIONS
 async function getData(url) {
   const res = await fetch(url);
   const data = await res.json();
   gallery.innerHTML = "";
   data.forEach((img) => {
-    console.log(img.urls.regular);
     gallery.innerHTML += `<div class="picture" style="background: url(${img.urls.small})" data-link="${img.urls.raw}"></div>`;
   });
   setListener();
 }
+async function getFindingImages(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+  gallery.innerHTML = "";
+  data.results.forEach((img) => {
+    gallery.innerHTML += `<div class="picture" style="background: url(${img.urls.small})" data-link="${img.urls.raw}"></div>`;
+  });
+  setListener();
+}
+function getSeason() {
+  return Math.round((date.getMonth() / 12) * 4) % 4;
+}
+
+// SET FUNCTIONS
 function setListener() {
   const listOfPic = document.querySelectorAll(".picture");
   listOfPic.forEach((picture) => {
@@ -31,28 +41,33 @@ function setListener() {
 }
 function setColorPalette(time) {
   if (time >= 5 && time <= 18) {
-    document.documentElement.style.setProperty('--main', 'black');
-    document.documentElement.style.setProperty('--second', 'white');
+    document.documentElement.style.setProperty("--main", "black");
+    document.documentElement.style.setProperty("--second", "white");
   } else {
-    document.documentElement.style.setProperty('--main', 'white');
-    document.documentElement.style.setProperty('--second', 'black');
+    document.documentElement.style.setProperty("--main", "white");
+    document.documentElement.style.setProperty("--second", "black");
   }
 }
-function getSeason() {
-  return Math.round((date.getMonth() / 12) * 4) % 4;
-}
+
 function findImages(keyword) {
-  const url = `https://api.unsplash.com/search/photos?page=1&query=${keyword}&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo`;
-  console.log(`finding images... by ${keyword}`);
-  getData(url);
+  const url = `https://api.unsplash.com/search/photos?&per_page=30&query=${keyword}&client_id=588sChbD4XYfVMX6oxBTVOtqczMXoFRFt2eYmCEkglk`;
+  getFindingImages(url);
 }
 
+// LISTENERS
 searchIcon.addEventListener("click", () => {
-  searchInput.value && findImages(searchInput.value);
+  !searchInput.value ? getData(url) : findImages(searchInput.value);
 });
 searchInput.addEventListener("keypress", (e) => {
-  searchInput.value && e.key === "Enter" && findImages(searchInput.value);
+  !searchInput.value
+    ? e.key === "Enter" && getData(url)
+    : e.key === "Enter" && findImages(searchInput.value);
+});
+seasonBtn.addEventListener("click", () => {
+  const season = arrayOfSeasons[getSeason()];
+  const url = `https://api.unsplash.com/search/photos?&per_page=30&query=${season}&client_id=588sChbD4XYfVMX6oxBTVOtqczMXoFRFt2eYmCEkglk`;
+  getFindingImages(url);
 });
 
 setColorPalette(date.getHours());
-getData(testUrl);
+getData(url);
